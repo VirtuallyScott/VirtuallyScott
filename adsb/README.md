3906afe3e6a73affb2959aeb7da5921072ab533d
Scott S <scott.smith@virtuallyscott.com>
2022-02-19 20:22:55 -0700

    update install script and readme

---
+++ adsb/README.md
@@ -0,0 +1,8 @@
+# Install ADSB to feed www.flightaware.com and www.flightradar24.com   
+---   
+- https://flightaware.com/adsb/piaware/install   
+- Claim Your Receiver https://flightaware.com/adsb/piaware/claim   
+- https://www.flightradar24.com/share-your-data   
+- Claim Your Receiver https://www.flightradar24.com/activate-raspberry-pi   
+
+
--- adsb/ads-b_installer.sh
+++ adsb/ads-b_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /tmp
-wget http://flightaware.com/adsb/piaware/files/packages/pool/piaware/p/piaware-support/piaware-repository_3.5.3_all.deb
-sudo dpkg -i ./piaware-repository_3.5.3_all.deb
+wget https://flightaware.com/adsb/piaware/files/packages/pool/piaware/p/piaware-support/piaware-repository_7.1_all.deb
+sudo dpkg -i piaware-repository_7.1_all.deb
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt auto-remove
@@ -9,6 +9,6 @@ sudo apt-get clean
 sudo apt-get install piaware -y
 sudo piaware-config allow-auto-updates yes
 sudo piaware-config allow-manual-updates yes
-sudo apt-get install dump1090-fa dirmngr -y
+sudo apt-get install dump1090-fa dirmngr dump978-fa -y
 sudo bash -c "$(wget -O - http://repo.feed.flightradar24.com/install_fr24_rpi.sh)"
 sudo reboot
